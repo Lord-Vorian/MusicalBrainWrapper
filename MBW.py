3a059f1f5104c8e2106618335efc7defeb7c6c6b
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-05-16 01:48:27 -0600

    simplified test() logic, functionality unchanged

--- MBW.py
+++ MBW.py
@@ -33,11 +33,10 @@ def fadeIn(player, seconds):
   :param seconds: Time to fade over
   :return: Nothing
   """
+  player.audio_set_volume(0)
+  player.play()
   seconds = float(seconds)
   step = seconds / 100
-  player.audio_set_volume(0)
-  # player.set_position(0.5)
-  player.play()
   while seconds > 0:
     player.audio_set_volume(100 - int(seconds/step))
     seconds -= step
@@ -62,7 +61,7 @@ def fadeOut(player, seconds):
 
 
 def test(player):
-  elapsed = 0
+  elapsed = 0 # not used, for timeout on test function?
   invasions = 0
   invading = 0
   while True:
@@ -70,21 +69,21 @@ def test(player):
     if not int(time()) % 5:
 
       if signaler.invader_detect():
-        invading = 2
-        if signaler.invader_detect() and invading <= 0:
+        if invading <= 0:
           player.set_media(instance.media_new(choice(cleaned_list)[0]))
           fadeIn(player, 2)
           print('New invasion # {}'.format(invasions))
           invasions += 1
+        else:
+          invading = 2
+      else:
+        invading -= 1
 
-      elif invading > 0 and not player.is_playing():
+      if invading > 0 and not player.is_playing():
         player.set_media(instance.media_new(choice(cleaned_list)[0]))
         fadeIn(player, 1)
       
-      elif not signaler.invader_detect():
-        invading -= 1
-
-      if invading <= 0 and player.is_playing():
+      elif invading <= 0 and player.is_playing():
         fadeOut(player, 5)
     sleep(1)
 
