60fc376bec0f63b30af0d1a5f56db39a8106c722
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-05-16 01:29:46 -0600

    shortened & sped up invader_detect()

--- signaler.py
+++ signaler.py
@@ -7,27 +7,24 @@ from time import time, sleep
 
 def invader_detect():
   """Takes a screenshot and looks for red clumps"""
-  image = screenshot()
-  red_count = 0
-  pink_count = 0
+  image = screenshot(); image.save('temp.png')
+  red_count, pink_count = 0, 0
   total = 0 # not used
+
   for pixel in image.getdata():
     total += 1 # not used
-    if pixel[0] > 100 and pixel[1] < 50 and pixel[2] < 50:
-      red_count += 1
+    if pixel[0] > 100 and pixel[1] < 50:
+      if pixel[2] < 50: red_count += 1
+      elif pixel[2] > 150: pink_count += 1
 
-    elif pixel[0] > 100 and pixel[1] < 50 and pixel[2] > 150:
-      pink_count += 1
+    if red_count > 1000:
+      print('Red Invader detected')
+      return True
+    elif pink_count > 25:
+      print('Pink Invader detected')
+      return True
+  return False
 
-  image.save('temp.png')
-  if red_count > 1000:
-    print('Red Invader detected')
-    return True
-  elif pink_count > 50:
-    print('Pink Invader detected')
-    return True
-  else:
-    return False
 
 def blink(interval=3600):
   """Run detection func at a set time interval"""
