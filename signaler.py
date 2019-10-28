1a3c13a7fc59a28275ad6b00673be8dbf13c5fb9
lord-vorian <highskieseverywhere@gmail.com>
2019-10-27 19:07:23 -0600

    signaler class for multi-thread

--- signaler.py
+++ signaler.py
@@ -4,30 +4,34 @@ This module(?) carries all of the monitoring functionality
 """
 from pyautogui import screenshot
 from time import time, sleep
+class Signaler:
 
-def invader_detect():
-  """Takes a screenshot and looks for red clumps"""
-  image = screenshot(); image.save('temp.png')
-  red_count, pink_count = 0, 0
-  total = 0 # not used
+  def __init__(self):
+    self.invader = False
 
-  for pixel in image.getdata():
-    total += 1 # not used
-    if pixel[0] > 100 and pixel[1] < 50:
-      if pixel[2] < 50: red_count += 1
-      elif pixel[2] > 150: pink_count += 1
+  def invader_detect(self):
+    """Takes a screenshot and looks for red clumps"""
+    image = screenshot(); image.save('temp.png')
+    red_count, pink_count = 0, 0
+    total = 0 # not used
 
-    if red_count > 1000:
-      print('Red Invader detected')
-      return True
-    elif pink_count > 25:
-      print('Pink Invader detected')
-      return True
-  return False
+    for pixel in image.getdata():
+      total += 1 # not used
+      if pixel[0] > 100 and pixel[1] < 50:
+        if pixel[2] < 50: red_count += 1
+        elif pixel[2] > 150: pink_count += 1
+
+      if red_count > 1000:
+        print('Red Invader detected')
+        return True
+      elif pink_count > 25:
+        print('Pink Invader detected')
+        return True
+    return False
 
 
-def blink(interval=3600):
-  """Run detection func at a set time interval"""
-  for elapsed in range(interval):
-    if not (int(time()) % 5): invader_detect()
-    sleep(1)
+  def blink(self, interval=3600):
+    """Run detection func at a set time interval"""
+    for elapsed in range(interval):
+      if not (int(time()) % 5): self.invader = self.invader_detect()
+      sleep(1)
