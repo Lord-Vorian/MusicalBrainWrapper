1587fa7581f312e9e88ef0d60513f32f36ecaaea
lord-vorian <highskieseverywhere@gmail.com>
2019-10-28 21:12:40 -0600

    moved class to brain.py instead of signaler.py

--- brain.py
+++ brain.py
@@ -3,8 +3,21 @@ Interfaces with OpenBCI Cyton board for live stream EEG data
 """
 
 from pyOpenBCI import cyton
+import signaler
+import threading
+
 
 board = cyton.OpenBCICyton(port='COM3')
+
+class Streamer:
+
+    def __init__(self, flagger):
+        self.flag = False
+        self.flagger = flagger
+
+    def update_flag(self):
+        flagger = threading.Thread(target=self.flagger.blink)
+
 
 def print_raw(sample):
     print(sample.channels_data)
--- signaler.py
+++ signaler.py
@@ -4,34 +4,30 @@ This module(?) carries all of the monitoring functionality
 """
 from pyautogui import screenshot
 from time import time, sleep
-class Signaler:
 
-  def __init__(self):
-    self.invader = False
+def invader_detect():
+  """Takes a screenshot and looks for red clumps"""
+  image = screenshot(); image.save('temp.png')
+  red_count, pink_count = 0, 0
+  total = 0 # not used
 
-  def invader_detect(self):
-    """Takes a screenshot and looks for red clumps"""
-    image = screenshot(); image.save('temp.png')
-    red_count, pink_count = 0, 0
-    total = 0 # not used
+  for pixel in image.getdata():
+    total += 1 # not used
+    if pixel[0] > 100 and pixel[1] < 50:
+      if pixel[2] < 50: red_count += 1
+      elif pixel[2] > 150: pink_count += 1
 
-    for pixel in image.getdata():
-      total += 1 # not used
-      if pixel[0] > 100 and pixel[1] < 50:
-        if pixel[2] < 50: red_count += 1
-        elif pixel[2] > 150: pink_count += 1
-
-      if red_count > 1000:
-        print('Red Invader detected')
-        return True
-      elif pink_count > 25:
-        print('Pink Invader detected')
-        return True
-    return False
+    if red_count > 1000:
+      print('Red Invader detected')
+      return True
+    elif pink_count > 25:
+      print('Pink Invader detected')
+      return True
+  return False
 
 
-  def blink(self, interval=3600):
-    """Run detection func at a set time interval"""
-    for elapsed in range(interval):
-      if not (int(time()) % 5): self.invader = self.invader_detect()
-      sleep(1)
+def blink(interval=3600):
+  """Run detection func at a set time interval"""
+  for elapsed in range(interval):
+    if not (int(time()) % 5): return invader_detect()
+    sleep(1)
